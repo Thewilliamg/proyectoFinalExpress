051966ce11970381d5c21421bb92906465f548bf
Jhon-GG <111620215+Jhon-GG@users.noreply.github.com>
2024-09-19 11:27:45 -0500

    feat: :bug: background color arrangement

--- src/pages/start/Start.jsx
+++ src/pages/start/Start.jsx
@@ -3,7 +3,7 @@ import ruraq_logo from '../../../public/img/ruraq-logo.svg'
 export default function User() {
     
     return (
-        <div className='startBack'>
+        <div className='startBack' style={{backgroundImage: `url('/public/img/start-background.svg')`}}>
             <div className='startLogo'>
                 <img src= {ruraq_logo}></img>
             </div>
