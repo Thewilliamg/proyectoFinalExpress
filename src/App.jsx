b5f2b0db644147e39b20087ae29859c815a94551
Jhon-GG <111620215+Jhon-GG@users.noreply.github.com>
2024-09-19 07:31:25 -0500

    feat: :construction: Finalization of the starting screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from './pages/home/Home.jsx';
+import Start from './pages/start/Start.jsx';
 import User from './pages/user/User.jsx';
 import Markets from './pages/markets/Markets.jsx';
 import Discounts from './pages/discounts/Discounts.jsx';
@@ -10,21 +11,23 @@ import Policy from './pages/signup/register/policy/Policy.jsx'
 import Register from './pages/signup/register/Register.jsx'
 
 
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
 
         <Route index element={<Home />} />
-        //Rutas de descuentos
+        {/* //Rutas de descuentos */}
+        <Route path="/start" element={<Start/>}/>
         <Route path="/discounts" element={<Discounts/>} />
-        //Rutas de tiendas
+        {/* //Rutas de tiendas */}
         <Route path="/markets" element={<Markets />} />
-        //Rutas de comprar
+        {/* //Rutas de comprar */}
         <Route path="/shop" element={<Shop />} />
-        //Rutas de perfil de usuario
+        {/* //Rutas de perfil de usuario */}
         <Route path='/user' element={<User />} />
-        //Rutas de registro
+        {/* //Rutas de registro */}
         <Route path='/signup' element={<SignUp />} />                       // Pantalla 2
         <Route path='/signup/register' element={<Register />} /> 
         <Route path='/signup/register/policy' element={<Policy />} />       // Ruta para pantallas 4 y 5
---
+++ src/pages/start/Start.jsx
@@ -0,0 +1,12 @@
+import './Start.css';
+import ruraq_logo from '../../../public/img/ruraq-logo.svg'
+export default function User() {
+    
+    return (
+        <div className='startBack'>
+            <div className='startLogo'>
+                <img src= {ruraq_logo}></img>
+            </div>
+        </div>
+    )
+}
