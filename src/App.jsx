bf7c38780df542a693644c5ddb8bae5e56f6cb09
Jhon-GG <111620215+Jhon-GG@users.noreply.github.com>
2024-09-19 07:31:25 -0500

    feat: :construction: Finalization of the starting screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from './pages/home/Home.jsx';
+import Start from './pages/start/Start.jsx';
 import User from './pages/user/User.jsx';
 import Markets from './pages/markets/Markets.jsx';
 import Discounts from './pages/discounts/Discounts.jsx';
@@ -11,6 +12,7 @@ import Register from './pages/signup/register/Register.jsx'
 import Service from './pages/home/components/customerService/Service.jsx'
 
 
+
 function App() {
   return (
     <BrowserRouter>
@@ -19,14 +21,16 @@ function App() {
         <Route index element={<Home />} />
         <Route path="/customer-service" element={<Service/> } />  //Ruta para pantalla 25
         //Rutas de descuentos
+        {/* //Rutas de descuentos */}
+        <Route path="/start" element={<Start/>}/>
         <Route path="/discounts" element={<Discounts/>} />
-        //Rutas de tiendas
+        {/* //Rutas de tiendas */}
         <Route path="/markets" element={<Markets />} />
-        //Rutas de comprar
+        {/* //Rutas de comprar */}
         <Route path="/shop" element={<Shop />} />
-        //Rutas de perfil de usuario
+        {/* //Rutas de perfil de usuario */}
         <Route path='/user' element={<User />} />
-        //Rutas de registro
+        {/* //Rutas de registro */}
         <Route path='/signup' element={<SignUp />} />                       // Pantalla 2
         <Route path='/signup/register' element={<Register />} /> 
         <Route path='/signup/register/policy' element={<Policy />} />       // Ruta para pantallas 4 y 5
