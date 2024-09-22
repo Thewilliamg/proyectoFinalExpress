7188885eb168319f39c971ded654e4e0dd3350e9
Jhon-GG <111620215+Jhon-GG@users.noreply.github.com>
2024-09-22 12:41:48 -0500

    feat: :lipstick: Fix some errors in signup and login style
    Fixing styles of signup and login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,48 +1,30 @@
 import './Login.css'
-import Facebook from '/public/img/facebookIcon.svg';
-import Instagram from '/public/img/instagramIcon.svg';
+import { Link } from "react-router-dom";
+import Linkedin from '/public/img/LinkedinIcon.svg';
 import Gmail from '/public/img/gmailIcon.svg';
 import Discord from '/public/img/discordIcon.svg';
 import User from '/public/img/UserIcon.svg';
 
-export default function Login(){
-    return(
+export default function SignUp() {
+    return (
         <div className="signupBack">
                 <div className="signupTitle">
-                    <h1>Inicia Sesión y<br/>
-                    continúa viendo <span className='signupTitle_2'>tus</span><br/> 
-                    <span className='signupTitle_2'>artesanías favoritas</span>
+                    <h1>Regístrate ahora y obtén <br/>
+                    las mejores promociones <br/> 
+                    en <span className='signupTitle_2'>artesanías colombianas</span>
                     </h1>
                 </div>
-                <div className="signupButtons">
-                    <a href="#" className='signupA'>
+                <div className="signupButtons2">
+                    <a className='signupA2'>
                         <button className='signup_btn'>
                             <span className='signupBtnSpan'>
-                                <img src= {Facebook} alt="Facebook">
+                                <img src= {Linkedin} alt="Linkdein">
                             </img>
                             </span>
-                            <p className='signup_p1'>Inicia sesión con <span className='signupBtnSpan2'>Facebook</span></p>
+                            <p className='signup_p1'>Inicia sesión con <span className='signupBtnSpan2'>Linkedin</span></p>
                         </button>
                     </a>
-                    <a href="#" className='signupA'>
-                        <button className='signup_btn'>
-                            <span className='signupBtnSpan'>
-                                <img src= {Instagram} alt="Instagram">
-                            </img>
-                            </span>
-                            <p className='signup_p1'>Inicia sesión con <span className='signupBtnSpan2'>Instagram</span></p>
-                        </button>
-                    </a>
-                    <a href="#" className='signupA'>
-                        <button className='signup_btn'>
-                            <span className='signupBtnSpan'>
-                                <img src= {Gmail} alt="Gmail">
-                            </img>
-                            </span>
-                            <p className='signup_p2'>Inicia sesión con <span className='signupBtnSpan2'>Gmail</span></p>
-                        </button>
-                    </a>
-                    <a href="#" className='signupA'>
+                    <a className='signupA2'>
                         <button className='signup_btn'>
                             <span className='signupBtnSpan'>
                                 <img src= {Discord} alt="Gmail">
@@ -51,16 +33,33 @@ export default function Login(){
                             <p className='signup_p2'>Inicia sesión con <span className='signupBtnSpan2'>Discord</span> </p>
                         </button>
                     </a>
-                    <a href="#" className='signupAll'>
+                    <a className='signupA'>
+                        <button className='signup_btn'>
+                            <span className='signupBtnSpan'>
+                                <img src= {Gmail} alt="Gmail">
+                            </img>
+                            </span>
+                            <p className='signup_p2'>Inicia sesión con <span className='signupBtnSpan2'>Gmail</span></p>
+                        </button>
+                    </a>
+                    <a className='signupAll2'>
                         <button className='signup_btn2'>
                             <span className='signupBtnSpan'>
                                 <img src= {User} alt="User">
                             </img>
                             </span>
-                            <p className='signup_p1'>Inicia sesión con <span className='signupBtnSpan2'>Ruraq Maki</span> </p>
+                            <p className='signup_p1'>Inicia sesión con tu cuenta de <span className='signupBtnSpan2'>ruraq maki</span> </p>
                         </button>
                     </a>
                 </div>
+                <div className="signupAccount2">
+                    <h1>
+                        ¿Ya tienes una cuenta?
+                    </h1>
+                </div>
+                <Link to="/login" className='signupAccount_link'>
+                    Inicia Sesión
+                </Link>
         </div>
     )
 }
