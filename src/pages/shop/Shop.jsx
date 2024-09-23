abe29f470b0974564430b098f61ceaa5beb625a6
Camilo-fd <156461877+Camilo-fd@users.noreply.github.com>
2024-09-23 12:07:14 -0500

    feat: :sparkles: Finished page number 29

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,26 +1,44 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Link } from "react-router-dom";
+import checkBoxTrue from '/img/checkBoxTrue.svg';
+import checkBoxFalse from '/img/checkBoxFalse.svg';
 import './shop.css';
 
 export default function Shop() {
     const [showPopup, setShowPopup] = useState(false);
 
-    const handleShowPopup = () => setShowPopup(true);
-    const handleClosePopup = () => setShowPopup(false);
+    const handleShowPopup = () => {
+        setShowPopup(true);
+    };
+
+    const handleClosePopup = () => {
+        setShowPopup(false);
+    };
 
     return (
         <div className='shop-container'>
             <button onClick={handleShowPopup} className="shop-button">Realizar Compra</button>
             {showPopup && (
-                <div className="popUp-shop">
-                    <div className='text-popUp'>
-                        <h4>¿Seguro de realizar la compra?</h4>
+                <>
+                    <div className="overlay"></div> {/* Fondo oscuro */}
+                    <div className="popUp-shop">
+                        <div className='text-popUp'>
+                            <h4>¿Seguro de realizar la compra?</h4>
+                        </div>
+                        <div className='button-popUp'>
+                            <button>
+                                <Link to="/shop/confirmation" className='link-popUp'>
+                                    <img src={checkBoxTrue} alt="" />
+                                    Si
+                                </Link>
+                            </button>
+                            <button onClick={handleClosePopup}>
+                                <img src={checkBoxFalse} alt="" />
+                                No
+                            </button>
+                        </div>
                     </div>
-                    <div className='button-popUp'>
-                        <button><Link to="/shop/confirmation" className='link-popUp'>Si</Link></button>
-                        <button onClick={handleClosePopup}>No</button>
-                    </div>
-                </div>
+                </>
             )}
         </div>
     );
