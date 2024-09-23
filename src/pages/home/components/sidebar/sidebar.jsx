dafd13c575b889608ec2079a84cf1ec0141fef79
Thewilliamg <156461507+Thewilliamg@users.noreply.github.com>
2024-09-23 12:43:53 -0500

    feat: :sparkles: Added links from sidebar

--- src/pages/home/components/sidebar/sidebar.jsx
+++ src/pages/home/components/sidebar/sidebar.jsx
@@ -28,8 +28,8 @@ export default function Sidebar({ isOpen}) {
         <nav className="nav-sidebar">
             <ul className="nav-list-sidebar">
               <li><img src={favoriteico} alt="favorico" /><a href="/favorites" className="nav-item-sidebar">Lista de favoritos</a></li>
-              <li><img src={shoppingico} alt="shopico" /><a href="#" className="nav-item-sidebar">Compras</a></li>
-              <li><img src={workscoursesico} alt="workcourseico" /><a href="#" className="nav-item-sidebar">Talleres</a></li>
+              <li><img src={shoppingico} alt="shopico" /><a href="/orders" className="nav-item-sidebar">Compras</a></li>
+              <li><img src={workscoursesico} alt="workcourseico" /><a href="/workshops" className="nav-item-sidebar">Talleres</a></li>
               <li><img src={couponico} alt="coupon" /><a href="coupons" className="nav-item-sidebar">Canjear cupon</a></li>
             </ul>
             <br/>
@@ -49,9 +49,9 @@ export default function Sidebar({ isOpen}) {
             </div>
             <br/>
             <ul className="nav-list-sidebar">
-              <li><img src={settingsico} alt="settIco" /><a href="#" className="nav-item-sidebar">Ajustes</a></li>
-              <li><img src={chatico} alt="chat" /><a href="#" className="nav-item-sidebar">Comentarios</a></li>
-              <li><img src={customerServico} alt="customerService" /><a href="#" className="nav-item-sidebar">Atencion al cliente</a></li>
+              <li><img src={settingsico} alt="settIco" /><a href="/settings" className="nav-item-sidebar">Ajustes</a></li>
+              <li><img src={chatico} alt="chat" /><a href="/community" className="nav-item-sidebar">Comentarios</a></li>
+              <li><img src={customerServico} alt="customerService" /><a href="customer_service" className="nav-item-sidebar">Atencion al cliente</a></li>
             </ul>
         </nav>
         <div className="sidebar-footer">
