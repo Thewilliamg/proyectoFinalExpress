f0651089a9a3a471a66504c48ccc96bea872eb6a
Camilo-fd <156461877+Camilo-fd@users.noreply.github.com>
2024-09-22 21:51:47 -0500

    feat: :construction: Creation of page number 29

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,8 +1,27 @@
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+import './shop.css';
+
 export default function Shop() {
+    const [showPopup, setShowPopup] = useState(false);
+
+    const handleShowPopup = () => setShowPopup(true);
+    const handleClosePopup = () => setShowPopup(false);
 
     return (
         <div className='shop-container'>
-            <h1>Shop</h1>
+            <button onClick={handleShowPopup} className="shop-button">Realizar Compra</button>
+            {showPopup && (
+                <div className="popUp-shop">
+                    <div className='text-popUp'>
+                        <h4>¿Seguro de realizar la compra?</h4>
+                    </div>
+                    <div className='button-popUp'>
+                        <button><Link to="/shop/confirmation" className='link-popUp'>Si</Link></button>
+                        <button onClick={handleClosePopup}>No</button>
+                    </div>
+                </div>
+            )}
         </div>
-    )
+    );
 }
