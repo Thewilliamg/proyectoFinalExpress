518b66eefa6aa7f8fad05a02e1f3eb2d27c8778a
Jhon-GG <111620215+Jhon-GG@users.noreply.github.com>
2024-09-25 09:40:26 -0500

    feat: :sparkles: Added validator docs from dev branch

--- api/routes/router.js
+++ api/routes/router.js
@@ -1,6 +1,17 @@
 const express = require('express');
-const { getUser } = require('../controllers/userController');
 const router = express.Router();
+const { getUser } = require('../controllers/userController');
+
+const userValidator = require('../validators/userValidator');
+const productValidator = require('../validators/userValidator');
+const agentValidator = require('../validators/userValidator');
+const categoryValidator = require('../validators/userValidator');
+const couponValidator = require('../validators/userValidator');
+const marketValidator = require('../validators/userValidator');
+const messageValidator = require('../validators/userValidator');
+const orderValidator = require('../validators/userValidator');
+const workshopValidator = require('../validators/userValidator');
+const shoppingCartValidator = require('../validators/userValidator');
 
 router.get('/:id', getUser);
 
