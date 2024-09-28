f9a3b47f485a0695a9a27884c74b81404409e870
Camilo-fd <156461877+Camilo-fd@users.noreply.github.com>
2024-09-28 18:10:21 -0500

    feat: :sparkles: Consume the API with the fetch to show the products with discounts depending on their category

--- src/pages/discounts/Discounts.jsx
+++ src/pages/discounts/Discounts.jsx
@@ -1,59 +1,70 @@
 import './discounts.css';
-import { useState} from 'react';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import square from '@/img/square-brown-figure.svg'
-import prueba from '@/img/imagenPrueba.svg'
 import DiscountFigure1 from './components/DiscountFigure1';
 import DiscountFigure2 from './components/DiscountFigure2';
 
 export default function Discounts() {
-
-    const discountItems = [
-        {
-          discount: "-30%",
-          image: prueba,
-          name: "Chalina Beige con flecos",
-          Price: "S/.100 S/.65",
-          artisan: "Asoc. de artesanos Tinkuy"
-        },
-        {
-            discount: "Envio Gratis",
-            image: prueba,
-            name: "Chalina Beige con flecos",
-            Price: "S/.100 S/.65",
-            artisan: "Asoc. de artesanos Tinkuy"
-          },
-          {
-            discount: "3x2",
-            image: prueba,
-            name: "Chalina Beige con flecos",
-            Price: "S/.100 S/.65",
-            artisan: "Asoc. de artesanos Tinkuy"
-          },
-          {
-            discount: "-30%",
-            image: prueba,
-            name: "Chalina Beige con flecos",
-            Price: "S/.100 S/.65",
-            artisan: "Asoc. de artesanos Tinkuy"
-          },
-          {
-            discount: "-30%",
-            image: prueba,
-            name: "Chalina Beige con flecos",
-            Price: "S/.100 S/.65",
-            artisan: "Asoc. de artesanos Tinkuy"
-          }
-        // Añade más objetos aquí para más items
-      ];
-
+    const [categories, setCategories] = useState([]);
     const [activeNavLink, setActiveNavLink] = useState(0);
+    const [discountItems, setDiscountItems] = useState([]);
+    const [filteredItems, setFilteredItems] = useState([]);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        fetchCategories();
+        fetchDiscountItems();
+    }, []);
+
+    useEffect(() => {
+        filterItemsByCategory();
+    }, [activeNavLink, discountItems]);
+
+    const fetchCategories = async () => {
+        try {
+            const response = await fetch('http://localhost:5001/api/discounts/category');
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            setCategories(data.data);
+        } catch (error) {
+            console.error('Error fetching categories:', error);
+        }
+    };
+
+    const fetchDiscountItems = async () => {
+        try {
+            const response = await fetch('http://localhost:5001/api/discounts');
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            setDiscountItems(data.data);
+        } catch (error) {
+            console.error('Error fetching discount items:', error);
+        }
+    };
 
     const handleNavLinkClick = (index) => {
         setActiveNavLink(index);
     };
 
+    const filterItemsByCategory = () => {
+        if (categories.length > 0 && discountItems.length > 0) {
+            const activeCategoryName = categories[activeNavLink].name;
+            const filtered = discountItems.filter(item => item.categoryData.name === activeCategoryName);
+            setFilteredItems(filtered);
+        }
+    };
+
+    const handleProductClick = (productId) => {
+        navigate(`/discounts/product/${productId}`);
+    };
+
     return (
-        <div className='discount-container'> {/*IMPORTANTE: Modificar Solo dentro de esta vista*/}
+        <div className='discount-container'>
             <div className='header-discount'>
                 <div className='headerImg-discount'>
                     <img src={square} alt="" />
@@ -65,16 +76,10 @@ export default function Discounts() {
             </div>
             <nav className="carrusel-discount">
                 <ul className="itemsCarrusel-discount">
-                    {discountItems.map((_, index) => (
-                        <li
-                            key={index}
-                            className={`navItem-discount ${index === activeNavLink ? 'majorNav-discount' : ''}`}
-                        >
-                            <button
-                                className='navLink-discount'
-                                onClick={() => handleNavLinkClick(index)}
-                            >
-                                Textileria
+                    {categories.map((category, index) => (
+                        <li key={index} className={`navItem-discount ${index === activeNavLink ? 'majorNav-discount' : ''}`} >
+                            <button className='navLink-discount' onClick={() => handleNavLinkClick(index)}>
+                                {category.name}
                             </button>
                         </li>
                     ))}
@@ -82,20 +87,20 @@ export default function Discounts() {
             </nav>
             <div className='infoItem-discount'>
                 <div className='containerInfo-discount'>
-                    {discountItems.map((item, index) => (
-                        <div className='subinfo-discount' key={index}>
+                    {filteredItems.map((item, index) => (
+                        <div className='subinfo-discount' key={index} onClick={() => handleProductClick(item._id)} style={{ cursor: 'pointer' }}>
                             {index % 2 === 0 ? (
-                                <DiscountFigure1 text={item.discount}/>
+                                <DiscountFigure1 text={`${item.discount}`}/>
                             ) : (
-                                <DiscountFigure2 text={item.discount}/>
+                                <DiscountFigure2 text={`${item.discount}`}/>
                             )}
                             <div className='imagenInfo-discount'>
-                                <img src={item.image} alt={item.name} />
+                                <img src={item.picture} alt={item.name} />
                             </div>
                             <div className='textInfo-discount'>
                                 <p>{item.name}</p>
-                                <p>{item.Price}</p>
-                                <p>{item.artisan}</p>
+                                <p>S/.{item.price}</p>
+                                <p>{item.marketData.name}</p>
                             </div>
                         </div>
                     ))}
