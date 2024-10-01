70e10dd47b7105905a18725e5a2e675357fe655f
Thewilliamg <156461507+Thewilliamg@users.noreply.github.com>
2024-10-01 07:37:50 -0500

    feat: :sparkles: Implemented into FrontEnd GetallProducts endpoint

--- src/pages/home/components/order.jsx
+++ src/pages/home/components/order.jsx
@@ -3,40 +3,49 @@ import Title from "../storage/img/Rectangle-86.svg";
 import Message from "../storage/img/message.svg";
 import { Link } from "react-router-dom";
 import Manta from "../storage/img/manta.svg";
-import {useState,useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
 export default function Order() {
   const userId = localStorage.getItem('userId');
-  const [shopped,setShopped] = useState();
+  const [shopped, setShopped] = useState();
+  const [allproducts, setAllProducts] = useState();
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch(`http://localhost:5001/api/orders/user/${userId}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+      .then(res => res.json())
+      .then(response =>
+        setShopped(response)
+      )
+      .catch((error) => {
+        console.error('Hubo un error:' + error.message);
+      });
+
+      fetch(`http://localhost:5001/api/products`, {
         method: 'GET',
         headers: {
-            'Content-Type': 'application/json',
+          'Content-Type': 'application/json',
         }
-    })
-    .then(res => res.json())
-    .then(response => 
-      setShopped(response)
-    )
-    .catch((error) => {
-        console.error('Hubo un error:' + error.message   );
-    });
-},[])
+      })
+        .then(res => res.json())
+        .then(response =>
+          setAllProducts(response)
+        )
+        .catch((error) => {
+          console.error('Hubo un error:' + error.message);
+        });
 
-  const items = {
-    img: Manta,
-    title: "Tapiz Chumpi Andino III",
-    price: "S/.600",
-    made: "Taller Awaq Ayllus",
-  };
+  }, [])
 
   return (
     <article className="orders-container">
       <div className="container-arrow">
         <Link to='/home'>
-          <GoBackArrow className='arrow'/>
+          <GoBackArrow className='arrow' />
         </Link>
 
         <img src={Title} className="img-title" />
@@ -46,10 +55,10 @@ export default function Order() {
         </div>
       </div>
       <div className="container-shopped">
-        {(shopped?.length===0) && (<p>No hay pedidos realizados por el usuario :c</p>) }
+        {(shopped?.length === 0) && (<p>No hay pedidos realizados por el usuario :c</p>)}
         {
-          shopped?.map((item,index)=>{
-            return(
+          shopped?.map((item, index) => {
+            return (
               <div className="card-shopped" key={item?.titleProduct}>
                 <img src={item?.imgProduct} className="img-product" />
 
@@ -57,7 +66,7 @@ export default function Order() {
                   <div className="container-text-shopped">
                     <p>{item?.titleProduct}</p>
                     <p>S/.{item?.priceProduct}</p>
-                    <p>{item?.measureProduct+', '+item?.weightProduct}</p>
+                    <p>{item?.measureProduct + ', ' + item?.weightProduct}</p>
                     <p>{item?.marketName}</p>
                   </div>
                   <Link>
@@ -78,96 +87,22 @@ export default function Order() {
       </div>
       <p className="edga">Sigue viendo más artesanías</p>
       <div className="container-artesan">
-        <div className="card-artesan">
-          <div className="cardet">
-            <img src={items.img} alt="" className="img-artesan" />
-            <div className="container-text-artesan">
-              <p>{items.title}</p>
-              <p>{items.price}</p>
-              <p>{items.made}</p>
-            </div>
-          </div>
-        </div>
-        <div className="card-artesan">
-          <div className="cardet">
-            <img src={items.img} alt="" className="img-artesan" />
-            <div className="container-text-artesan">
-              <p>{items.title}</p>
-              <p>{items.price}</p>
-              <p>{items.made}</p>
-            </div>
-          </div>
-        </div>
-        <div className="card-artesan">
-          <div className="cardet">
-            <img src={items.img} alt="" className="img-artesan" />
-            <div className="container-text-artesan">
-              <p>{items.title}</p>
-              <p>{items.price}</p>
-              <p>{items.made}</p>
-            </div>
-          </div>
-        </div>
-        <div className="card-artesan">
-          <div className="cardet">
-            <img src={items.img} alt="" className="img-artesan" />
-            <div className="container-text-artesan">
-              <p>{items.title}</p>
-              <p>{items.price}</p>
-              <p>{items.made}</p>
-            </div>
-          </div>
-        </div>
-        <div className="card-artesan">
-          <div className="cardet">
-            <img src={items.img} alt="" className="img-artesan" />
-            <div className="container-text-artesan">
-              <p>{items.title}</p>
-              <p>{items.price}</p>
-              <p>{items.made}</p>
-            </div>
-          </div>
-        </div>
-        <div className="card-artesan">
-          <div className="cardet">
-            <img src={items.img} alt="" className="img-artesan" />
-            <div className="container-text-artesan">
-              <p>{items.title}</p>
-              <p>{items.price}</p>
-              <p>{items.made}</p>
-            </div>
-          </div>
-        </div>
-        <div className="card-artesan">
-          <div className="cardet">
-            <img src={items.img} alt="" className="img-artesan" />
-            <div className="container-text-artesan">
-              <p>{items.title}</p>
-              <p>{items.price}</p>
-              <p>{items.made}</p>
-            </div>
-          </div>
-        </div>
-        <div className="card-artesan">
-          <div className="cardet">
-            <img src={items.img} alt="" className="img-artesan" />
-            <div className="container-text-artesan">
-              <p>{items.title}</p>
-              <p>{items.price}</p>
-              <p>{items.made}</p>
-            </div>
-          </div>
-        </div>
-        <div className="card-artesan">
-          <div className="cardet">
-            <img src={items.img} alt="" className="img-artesan" />
-            <div className="container-text-artesan">
-              <p>{items.title}</p>
-              <p>{items.price}</p>
-              <p>{items.made}</p>
-            </div>
-          </div>
-        </div>
+        {
+          allproducts?.map((items, index) => {
+            return (
+              <div className="card-artesan">
+                <div className="cardet">
+                  <img src={items.picture} alt="" className="img-artesan" />
+                  <div className="container-text-artesan">
+                    <p>{items.name}</p>
+                    <p>S/.{items.price}</p>
+                    <p>{items.marketName}</p>
+                  </div>
+                </div>
+              </div>
+            )
+          })
+        }
       </div>
     </article>
   );
